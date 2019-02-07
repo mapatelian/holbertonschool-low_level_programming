2e43f61b2f71abd0ed209021c1bc3a748dd2e42a
mapatelian <alex.mapatelian@gmail.com>
2019-02-07 06:51:09 +0000

    program that prints a string followed by a new line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints a string
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
