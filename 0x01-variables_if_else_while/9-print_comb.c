2d19a96c35c2beda39384bdd5d2a7048af2efa06
mapatelian <alex.mapatelian@gmail.com>
2019-02-08 06:44:17 +0000

    moved the if statement around

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,15 +13,17 @@ int main(void)
 
 	for (i = '0'; i <= '9'; i++)
 	{
-		if (i != '9')
+		putchar(i);
+
+		if (i < '9')
 		{
-			putchar(i);
 			putchar(',');
 			putchar(' ');
 		}
-		else
-			putchar(i);
+
 	}
+
+	putchar('\n');
 
 	return (0);
 }
