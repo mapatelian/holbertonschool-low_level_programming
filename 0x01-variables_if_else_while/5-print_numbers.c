297c7e28324d2831c1355e428727984b5445c2bd
mapatelian <alex.mapatelian@gmail.com>
2019-02-08 05:52:06 +0000

    function that prints numbers of the base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers of the base 10
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+
+}
