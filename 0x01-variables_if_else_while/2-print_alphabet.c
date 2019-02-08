71a85749b56ed7c0d40aae2196eb4b1d01625b9b
mapatelian <alex.mapatelian@gmail.com>
2019-02-08 05:29:08 +0000

    function that prints alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	putchar('\n');
+
+	return (0);
+}
