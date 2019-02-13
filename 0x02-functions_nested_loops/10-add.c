780ec88f9b8aa97820210b569dbf55500e1979bd
mapatelian <alex.mapatelian@gmail.com>
2019-02-13 07:12:19 +0000

    function that adds two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * add - adds two integers
+ *
+ * @n: first integer
+ * @k: second integer
+ *
+ * Return: sum of the 2 integers
+ */
+
+int add(int n, int k)
+{
+
+	int result;
+
+	result = n + k;
+
+	return (result);
+}
