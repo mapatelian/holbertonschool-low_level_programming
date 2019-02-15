b211d6872ee5afbf281df482ea062144b3e5f38e
mapatelian <alex.mapatelian@gmail.com>
2019-02-15 02:13:52 +0000

    added a new line character

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,13 +11,13 @@ int main(void)
 
 	int i;
 
-	for (i = 1; i <= 101; i++)
+	for (i = 1; i <= 100; i++)
 	{
 
 		if ((i % 3 == 0) && (i % 5 == 0))
 			printf("FizzBuzz ");
 		else if (i == 100)
-			printf("Buzz\n");
+			printf("Buzz");
 		else if (i % 3 == 0)
 			printf("Fizz ");
 		else if (i % 5 == 0)
@@ -25,5 +25,7 @@ int main(void)
 		else
 			printf("%i ", i);
 	}
+	printf("\n");
+
 	return (0);
 }
