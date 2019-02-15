64a3baa1615b0e2285a2fec7e62173ea4af865b0
mapatelian <alex.mapatelian@gmail.com>
2019-02-15 02:09:00 +0000

    Fizz&Buzz

---
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Fizz&Buzz
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+
+	int i;
+
+	for (i = 1; i <= 101; i++)
+	{
+
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf("FizzBuzz ");
+		else if (i == 100)
+			printf("Buzz\n");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%i ", i);
+	}
+	return (0);
+}
