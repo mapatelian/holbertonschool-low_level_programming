a27e895721cadfd4b6acd554271c23eb6cd73ce6
mapatelian <alex.mapatelian@gmail.com>
2019-02-20 03:49:07 +0000

    added a null byte

--- 0x04-pointers_arrays_strings/8-print_array.c
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -14,10 +14,11 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		if (i < (n - 1))
+		if (i < n - 1)
 			printf("%i, ", a[i]);
 
 		else
 			printf("%i\n", a[i]);
 	}
+	a[n] = '\0';
 }
