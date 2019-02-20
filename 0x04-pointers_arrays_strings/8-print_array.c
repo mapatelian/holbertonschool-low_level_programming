56d2315e6690fe8e393b4cb8baf9caad2895de83
mapatelian <alex.mapatelian@gmail.com>
2019-02-20 03:22:57 +0000

    function that prints a certain number of elements of an array

---
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_array - prints elements of an array
+ *
+ * @a: pointer to an array
+ * @n: number of elements to be printed
+ */
+
+void print_array(int *a, int n)
+{
+
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i < (n - 1))
+			printf("%i, ", a[i]);
+
+		else
+			printf("%i\n", a[i]);
+	}
+}
