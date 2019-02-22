366a830043cfbf5342a8ebb9ba13a91ef2359b74
mapatelian <alex.mapatelian@gmail.com>
2019-02-22 23:10:26 +0000

    function that prints an integer

---
+++ 0x05-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_number - prints numbers recursively
+ *
+ * @n: number to be printed
+ */
+
+void print_number(int n)
+{
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10)
+		print_number(n / 10);
+
+	_putchar((n % 10) + '0');
+
+}
