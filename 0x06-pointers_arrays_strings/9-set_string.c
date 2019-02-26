a611d35329b9380824e29196e084d33e02a3e794
mapatelian <alex.mapatelian@gmail.com>
2019-02-26 10:30:51 +0000

    function that sets the value of a pointer to a char

---
+++ 0x06-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,14 @@
+#include "holbereton.h"
+
+/**
+ * set_string - sets the value of a pointer to a char
+ *
+ * @s: string to be modified
+ * @to: string to be set
+ */
+
+void set_string(char **s, char *to)
+{
+
+	*s = to;
+}
