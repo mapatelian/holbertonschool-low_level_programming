e3a06a91a7e66da8a382b73453776231070cb2b2
Sasha <alex.mapatelian@gmail.com>
2019-03-06 07:22:48 +0000

    function that creates a 2D array

---
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * alloc_grid - creates a 2D array
+ *
+ * @width: width of the array
+ * @height: height of the array
+ *
+ * Return: on success - pointer to the 2D array, otherwise - NULL
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **table;
+	int i;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	table = malloc(height * sizeof(int));
+
+	if (table == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		table[i] = malloc(sizeof(int) * width);
+		if (table == NULL)
+			return (NULL);
+	}
+
+	return (table);
+}
