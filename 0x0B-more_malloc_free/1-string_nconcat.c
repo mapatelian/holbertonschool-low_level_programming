9653990ce39b26cd55fe7dbc4e4f3476c65f0c56
Sasha <alex.mapatelian@gmail.com>
2019-03-07 23:34:02 +0000

    fixed a case if a string is NULL

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -18,29 +18,25 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (!s1 && !s2)
 		return (NULL);
-
 	if (s1)
 		for (first = 0; s1[first]; first++)
 		;
+	else
+		first = 0;
 	if (s2)
 		for (second = 0; s2[second]; second++)
 		;
+	else
+		second = 0;
 	if (n >= second)
 		n = second;
-
 	lenght = first + n;
-
 	copy = malloc(sizeof(char) * (lenght + 1));
-
 	if (!copy)
 		return (NULL);
-
 	for (i = 0; i < first; i++)
-	{
 		copy[i] = s1[i];
-	}
 	k = 0;
-
 	for (; i < lenght; i++)
 	{
 		copy[i] = s2[k];
