1bd28f5bb2665fe9c1dd6ded92102f0868fa3783
Sasha <alex.mapatelian@gmail.com>
2019-03-12 05:27:08 +0000

    header file that defines a struct

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOGGY_H_
+#define _DOGGY_H_
+
+/**
+ * struct dog - Short description
+ * @name: name of the dog
+ * @age: age of the gog
+ * @owner: name of the owner
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} my_dog;
+
+#endif
