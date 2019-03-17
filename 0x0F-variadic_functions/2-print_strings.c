a1ee31a1fb59a73b1fd245aaea4128d477179938
Sasha <alex.mapatelian@gmail.com>
2019-03-17 22:33:33 +0000

    function that prints parameters

---
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings
+ *
+ * @separator: separator
+ * @n: number of parameters
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list valist;
+	size_t i;
+	char *string;
+
+	va_start(valist, n);
+
+	for (i = 0; i < n; i++)
+	{
+		string = va_arg(valist, char*);
+		if (string == 0)
+		string = "(nil)";
+
+		printf("%s", string);
+
+		if (i != n - 1)
+		printf("%s", separator);
+	}
+	printf("\n");
+	va_end(valist);
+}
