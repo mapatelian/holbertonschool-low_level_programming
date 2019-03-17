56469230dc1ead100bbcdacd4457b33eedb0d6fa
Sasha <alex.mapatelian@gmail.com>
2019-03-17 06:40:52 +0000

    function that summs up the parameters

---
+++ 0x0F-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sums up the parameters
+ *
+ * @n: number of parameters
+ *
+ * Return: 0 if n == 0
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list valist;
+	int sum = 0;
+	unsigned int i;
+
+	if (!n)
+		return (0);
+
+	va_start(valist, n);
+
+	for (i = 0; i < n; i++)
+	{
+		sum = sum + va_arg(valist, int);
+	}
+
+	va_end(valist);
+
+	return (sum);
+
+}
