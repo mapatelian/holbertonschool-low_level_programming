995c81ae5f8fe01784c893397164f704038d3d1f
Sasha <alex.mapatelian@gmail.com>
2019-05-03 07:52:24 +0000

    function that prints number of elements in a linked list

---
+++ 0x16-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints number of elements in a linked list
+ *
+ * @h: pointer to a head of a linked list
+ *
+ * Return: number of elements
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		i++;
+		h = h->next;
+	}
+
+	return (i);
+}
