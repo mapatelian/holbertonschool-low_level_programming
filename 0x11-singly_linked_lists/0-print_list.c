f5a6d54df18cad33237118aa3a2d61c0e400cd2c
Sasha <alex.mapatelian@gmail.com>
2019-05-23 08:06:02 +0000

    added _strlen

--- 0x11-singly_linked_lists/0-print_list.c
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -14,11 +14,27 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (h->str)
-			printf("[%lu] %s\n", strlen(h->str), h->str);
+			printf("[%i] %s\n", _strlen(h->str), h->str);
 		else
 			printf("[0] (nil)\n");
 		h = h->next;
 		i++;
 	}
+	return (i);
+}
+
+/**
+ * _strlen - counts the number of characters in a string
+ * @string: stored string
+ * Return: int number of characters in string
+ */
+
+int _strlen(char *string)
+{
+	int i;
+
+	for (i = 0; i[string]; i++)
+	;
+
 	return (i);
 }
