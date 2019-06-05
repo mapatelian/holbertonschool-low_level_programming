0f52a7043e6c0b5c077c996e773128aefecacdf2
Sasha <alex.mapatelian@gmail.com>
2019-06-05 06:18:22 +0000

    fixed if statement: key can be empty

--- 0x19-hash_tables/2-key_index.c
+++ 0x19-hash_tables/2-key_index.c
@@ -13,7 +13,7 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int hash, index;
 
-	if (!key || !*key || !size)
+	if (!size)
 		return (EXIT_FAILURE);
 
 	hash = hash_djb2(key);
