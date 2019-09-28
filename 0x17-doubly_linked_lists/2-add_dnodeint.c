3f91a6a1a5f84d6ff7a24f4f5f19239cf5a6e454
Sasha <alex.mapatelian@gmail.com>
2019-09-28 10:03:42 +0000

    added an if statement

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -13,20 +13,25 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
 
-	new = malloc(sizeof(dlistint_t));
+	if (head)
+	{
+		new = malloc(sizeof(dlistint_t));
 
-	if (!new)
-		return (NULL);
+		if (!new)
+			return (NULL);
 
-	new->n = n;
-	new->next = *head;
+		new->n = n;
+		new->next = *head;
 
-	if (*head != NULL)
-		new->prev = *head;
-	else
-		new->prev = NULL;
+		if (*head != NULL)
+			new->prev = *head;
+		else
+			new->prev = NULL;
 
-	*head = new;
+		*head = new;
 
-	return (new);
+		return (new);
+	}
+
+	return (NULL);
 }
