508a979660f38edbef61b21fbd39781ff83cd100
Sasha <alex.mapatelian@gmail.com>
2019-10-12 05:12:06 +0000

    fixed the algorithm

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,40 +1,6 @@
 #include "lists.h"
 
 /**
- * _strdup - duplicates a string
- *
- * @str: pointer to a string to be copied
- *
- * Return: pointer to a string if success, NULL is malloc fails
- */
-
-char *_strdup(const char *str)
-{
-	char *copy;
-	int i = 0;
-	int lenght = 0;
-
-	if (str == NULL)
-		return (NULL);
-
-	while (str[lenght])
-		lenght++;
-
-	copy = malloc(sizeof(char) * (lenght + 1));
-
-	if (copy == NULL)
-		return (NULL);
-
-	while (i < lenght)
-	{
-		copy[i] = str[i];
-		i++;
-	}
-	copy[i] = '\0';
-	return (copy);
-}
-
-/**
  * add_node_end - adds a new node at the end of a linked list
  *
  * @head: pointer to a head of the list
@@ -46,26 +12,30 @@ char *_strdup(const char *str)
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new, *copy;
+	int lenght = 0;
 
 	copy = *head;
 
 	new = malloc(sizeof(list_t));
+
 	if (!new)
 	{
 		free(new);
 		return (NULL);
 	}
 
-	new->str = _strdup(str);
-	
-	if (!new->str)
-	{
-		free(new->str);
-		return (NULL);
-	}
+	while (str[lenght])
+		lenght++;
+
+	new->str = strdup(str);
+	new->len = lenght;
 	new->next = NULL;
-	if (*head == NULL)
-		*head = new;
+
+	if ((*head) == NULL)
+	{
+		new->next = (*head);
+		(*head) = new;
+	}
 
 	else
 	{
@@ -74,6 +44,5 @@ list_t *add_node_end(list_t **head, const char *str)
 
 		copy->next = new;
 	}
-
-	return (copy);
+	return (new);
 }
