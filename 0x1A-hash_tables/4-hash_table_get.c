32aa220d85d8a8bec664c3986cf9fd55381772e9
Sasha <alex.mapatelian@gmail.com>
2019-10-15 11:12:32 +0000

    added functionality to look for a key in the table with collisions

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,6 +1,26 @@
 #include "hash_tables.h"
 
 /**
+ * list_len - checks how many elements in a linked list
+ *
+ * @h: pointer to a head of the list
+ *
+ * Return: number of elements
+ */
+
+size_t list_len(hash_node_t *h)
+{
+	int i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
+
+/**
  * hash_table_get - retrieves a value of a key
  * @ht: pointer to a hash table
  * @key: key to be checked
@@ -18,9 +38,16 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 		{
 			if (ht->array[i])
 			{
-				if ((_strncmpr(ht->array[i]->key, key) == 0))
+				if (list_len(ht->array[i]) == 1)
 				{
-					return (ht->array[i]->value);
+					if ((_strncmpr(ht->array[i]->key, key) == 0))
+						return (ht->array[i]->value);
+				}
+				while (ht->array[i])
+				{
+					if ((_strncmpr(ht->array[i]->key, key) == 0))
+						return (ht->array[i]->value);
+					ht->array[i] = ht->array[i]->next;
 				}
 			}
 		}
