0e6b028cb7f7e0bfafc239ec29c8eb44c87adfcf
Sasha <alex.mapatelian@gmail.com>
2019-10-18 09:22:51 +0000

    one more time

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -18,17 +18,14 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (!new)
 		return (NULL);
 
-	if (head == NULL)
-	{
-		*head = new;
-		new->prev = NULL;
-		new->n = n;
-		new->next = NULL;
-	}
-
+	new->prev = NULL;
 	new->n = n;
 	new->next = *head;
-	new->prev = *head;
+
+	if (*head)
+	{
+		(*head)->prev = new;
+	}
 	*head = new;
 
 	return (new);
